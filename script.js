97a092d9db5f00f411bf928c078841d908e29790
MaduDev003 <mariabevenuto062@gmail.com>
2025-11-14 11:42:53 -0300

    feat: assets folder

--- script.js
+++ script.js
@@ -4,15 +4,12 @@ const southWest = L.latLng(-90, -180);
 const northEast = L.latLng(90, 180);
 const worldBounds = L.latLngBounds(southWest, northEast);
 
-// ----------------------------------------------------
-// ALTERAÇÃO AQUI: Trocando o provedor para CartoDB Positron
-// ----------------------------------------------------
 L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png', {
     maxZoom: 19, 
     minZoom: 3,
     noWrap: true,
     bounds: worldBounds, 
-    // Mantenha ou atualize a atribuição para incluir o provedor
+
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="https://carto.com/attributions">CARTO</a>'
 }).addTo(map);
 
